8ce668f4ff7c5a13d78ced3e4bcc64e14a637259
Danish124 <114099183+Danish124@users.noreply.github.com>
2024-08-22 23:40:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web1" {
   ami               = var.instance_detail.ami
   instance_type     = var.instance_detail.instance_type
-  get_password_data = var.pass
+  # get_password_data = var.pass
 
   tags = var.ec2_resource
 }
