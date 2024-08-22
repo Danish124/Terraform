3144aaf7c66e2725ef41dcc4df8f1eb38e67a0bc
Danish124 <114099183+Danish124@users.noreply.github.com>
2024-08-23 02:20:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web1" {
+resource "aws_instance" "web" {
   ami               = var.instance_detail.ami
   instance_type     = var.instance_detail.instance_type
   # get_password_data = var.pass
